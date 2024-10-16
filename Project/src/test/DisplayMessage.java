b0c599f31c07f040e68dabf08311d6fc4a908f42
muham <muham@10.106.23.169>
2024-10-16 20:31:59 +0800

    Lab 1 quiz

---
+++ Project/src/test/DisplayMessage.java
@@ -0,0 +1,12 @@
+package test;
+
+public class DisplayMessage
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Java");
+		System.out.println("");
+		System.out.println("Learning Java now");
+		System.out.println("Programming is fun");
+	}
+}
---
+++ Project/src/test/Name.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Name
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Muhamad Towa");
+		System.out.println("227315");
+		System.out.println("Subang jaya");
+	}
+}
